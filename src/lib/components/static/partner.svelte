75c0a541d4b026daea1ff09dad6e318748078305
Chianli Hang <chianli@lanternfly.io>
2023-11-06 12:23:32 -0500

    partner picture

--- src/lib/components/static/partner.svelte
+++ src/lib/components/static/partner.svelte
@@ -1,5 +1,6 @@
-<div id="partner" class="fitScreen background-secondary textColor-primary">
+<div id="partner" class="fitScreen background-secondary textColor-primary relative">
     <h2>Partner</h2>
+    <img id="partnerImg" src="/img/partner.jpg" alt="Partnerships">
 </div>
 
 <style>
@@ -7,5 +8,13 @@
         background-color: var(--red);
         box-sizing: border-box;
         padding: 7.5vw;
+    }
+
+    #partnerImg{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 65vh;
     }
 </style>
